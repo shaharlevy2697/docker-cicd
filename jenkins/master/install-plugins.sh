41921aa7fa1cb2493af095bba1b77a2e695d0de7
shaharlevy2697 <61241719+shaharlevy2697@users.noreply.github.com>
2024-10-18 13:18:54 +0300

    Update install-plugins.sh

--- jenkins/master/install-plugins.sh
+++ jenkins/master/install-plugins.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -e
 
+# Directory to store Jenkins plugins
 REF_DIR="/usr/share/jenkins/ref/plugins"
 mkdir -p $REF_DIR
 
+# Function to install a plugin if it doesn't already exist
 installPlugin() {
   if [ ! -f "$REF_DIR/$1.jpi" ]; then
+    echo "Installing plugin: $1 version: $2"
     curl -L --silent --show-error -f https://updates.jenkins.io/download/plugins/$1/$2/$1.hpi -o "$REF_DIR/$1.jpi"
+  else
+    echo "Plugin $1 is already installed."
   fi
 }
 
+# Install required plugins with specified versions
 installPlugin git 4.7.2
 installPlugin matrix-auth 2.6.8
 installPlugin workflow-aggregator 2.6
